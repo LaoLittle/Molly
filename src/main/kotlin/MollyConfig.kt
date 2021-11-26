8eb50291fe4d4011104150f47baa2a26207ad8c7
LaoLittle <y1941803875@outlook.com>
2021-11-26 18:51:38 +0800

    Changed

--- src/main/kotlin/MollyConfig.kt
+++ src/main/kotlin/MollyConfig.kt
@@ -7,6 +7,7 @@ import net.mamoe.mirai.console.data.value
 object MollyConfig : AutoSavePluginConfig("MollyConfig") {
     @ValueDescription("在此填入你的Api-Key")
     val api_key: String by value("")
+
     @ValueDescription("在此填入你的Api-Secret")
     val api_secret: String by value("")
 }
