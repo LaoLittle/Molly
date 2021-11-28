006e8a6233ff3594d3f31b52a7a0f0646bd67cc6
LaoLittle <y1941803875@outlook.com>
2021-11-28 10:28:11 +0800

    删减非必要代码

--- src/main/kotlin/utils/Convert.kt
+++ src/main/kotlin/utils/Convert.kt
@@ -1,29 +1,21 @@
-@file:Suppress("unused", "unchecked")
-
 package org.laolittle.plugin.molly.utils
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.withContext
-import net.mamoe.mirai.contact.Contact
-import net.mamoe.mirai.event.events.MessageEvent
 
 class ConversationBuilder(
-    val eventContext: MessageEvent,
     val conversationBlock: suspend ConversationBuilder.() -> Unit
 ) {
-    private val target: Contact = eventContext.subject
-
     suspend operator fun invoke() = conversationBlock()
 }
 
-suspend fun <T : MessageEvent> T.conversation(
+suspend fun conversation(
     scope: CoroutineScope,
     block: suspend ConversationBuilder.() -> Unit
 ): ConversationBuilder {
     suspend fun execute() = ConversationBuilder(
-            eventContext = this@conversation,
-            conversationBlock = block
-        ).also { it() }
+        conversationBlock = block
+    ).also { it() }
 
     return withContext(scope.coroutineContext + scope.coroutineContext) { execute() }
 }
