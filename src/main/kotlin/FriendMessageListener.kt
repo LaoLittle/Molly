5a277491b608f87285164fea2b2f47641b6ced61
LaoLittle <y1941803875@outlook.com>
2021-12-18 23:19:51 +0800

    私聊无需发送”聊天“

--- src/main/kotlin/FriendMessageListener.kt
+++ src/main/kotlin/FriendMessageListener.kt
@@ -3,7 +3,8 @@ package org.laolittle.plugin.molly
 import kotlinx.serialization.ExperimentalSerializationApi
 import net.mamoe.mirai.event.GlobalEventChannel
 import net.mamoe.mirai.event.subscribeFriendMessages
-import net.mamoe.mirai.event.whileSelectMessages
+import net.mamoe.mirai.message.data.content
+import org.laolittle.plugin.molly.MollyConfig.dontReply
 import org.laolittle.plugin.molly.model.MollyApiService.mollyReply
 import org.laolittle.plugin.molly.model.MollyApiService.request
 import org.laolittle.plugin.molly.model.Reply.reply
@@ -12,35 +13,50 @@ import org.laolittle.plugin.molly.model.Reply.reply
 object FriendMessageListener : Service() {
     override suspend fun main() {
         GlobalEventChannel.subscribeFriendMessages {
-            finding(Regex("聊天")) {
-                subject.sendMessage("在呢")
-                var loop = true
-                while (loop)
-                    whileSelectMessages {
-                        default {
-                            request(
-                                message = it,
-                                userId = sender.id,
-                                userName = senderName,
-                                groupName = null,
-                                groupId = null,
-                                false
-                            )
-                            reply(this@FriendMessageListener, mollyReply)
-                            false
-                        }
-                        startsWith("不聊了") {
-                            subject.sendMessage("好吧")
-                            loop = false
-                            false
-                        }
-                        timeout(30_000) {
-                            subject.sendMessage("不聊了么？那我走了")
-                            loop = false
-                            false
-                        }
-                    }
+            always {
+                dontReply.forEach { dontNode -> if (message.content.contains(Regex(dontNode))) return@always }
+
+                request(
+                    message = it,
+                    userId = sender.id,
+                    userName = senderName,
+                    groupName = null,
+                    groupId = null,
+                    false
+                )
+                reply(this@FriendMessageListener, mollyReply)
             }
         }
+        /*     finding(Regex("聊天")) {
+                 subject.sendMessage("在呢")
+                 var loop = true
+                 while (loop)
+                     whileSelectMessages {
+                         default {
+                             request(
+                                 message = it,
+                                 userId = sender.id,
+                                 userName = senderName,
+                                 groupName = null,
+                                 groupId = null,
+                                 false
+                             )
+                             reply(this@FriendMessageListener, mollyReply)
+                             false
+                         }
+                         startsWith("不聊了") {
+                             subject.sendMessage("好吧")
+                             loop = false
+                             false
+                         }
+                         timeout(30_000) {
+                             subject.sendMessage("不聊了么？那我走了")
+                             loop = false
+                             false
+                         }
+                     }
+             }
+         }
+     } */
     }
 }
