db4d487604d122be4893662f1ac76c84a00ffb6d
LaoLittle <y1941803875@outlook.com>
2022-01-27 09:34:41 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
-    id("net.mamoe.mirai-console") version "2.9.0-M1"
+    id("net.mamoe.mirai-console") version "2.10.0-RC2"
 }
 
 group = "org.laolittle.plugin.molly"
-version = "1.1.0"
+version = "1.2.0"
 
 repositories {
-    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/central")
     mavenCentral()
     maven {
         url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
